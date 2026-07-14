ea77751f98d002c8e4d8517fb47683e8a9e6c904
AllenJohnn <allenjohnjoy2004@gmail.com>
2026-07-15 00:50:02 +0530

    dev done

--- src/components/AddData.jsx
+++ src/components/AddData.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import Nav from "./Nav";
+import NavBar from "./NavBar";
 
 const AddData = () => {
   const [input, changeInput] = useState({
@@ -24,13 +24,10 @@ const AddData = () => {
   };
 
   const readValue = () => {
-    console.log(input);
-
     axios
       .post("http://localhost:3000/add-data", input)
       .then((response) => {
-        console.log(response.data);
-        alert("Volunteer Added Successfully");
+        alert(response.data.status);
 
         changeInput({
           VolunteerID: "",
@@ -56,164 +53,159 @@ const AddData = () => {
 
   return (
     <div>
-      <Nav />
+      <NavBar />
+
       <div className="container mt-4">
-        <div className="row">
-          <div className="col-12">
-            <h2 className="text-center mb-4">
-              <b>ADD VOLUNTEER</b>
-            </h2>
+        <h3 className="text-center mb-4">Add Volunteer</h3>
 
-            <div className="row g-3">
-              <div className="col-md-6">
-                <label className="form-label">Volunteer ID</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="VolunteerID"
-                  value={input.VolunteerID}
-                  onChange={inputHandler}
-                />
-              </div>
+        <div className="row g-3">
+          <div className="col-md-6">
+            <label className="form-label">Volunteer ID</label>
+            <input
+              type="text"
+              className="form-control"
+              name="VolunteerID"
+              value={input.VolunteerID}
+              onChange={inputHandler}
+            />
+          </div>
 
-              <div className="col-md-6">
-                <label className="form-label">Full Name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="FullName"
-                  value={input.FullName}
-                  onChange={inputHandler}
-                />
-              </div>
+          <div className="col-md-6">
+            <label className="form-label">Full Name</label>
+            <input
+              type="text"
+              className="form-control"
+              name="FullName"
+              value={input.FullName}
+              onChange={inputHandler}
+            />
+          </div>
 
-              <div className="col-md-6">
-                <label className="form-label">Email</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  name="Email"
-                  value={input.Email}
-                  onChange={inputHandler}
-                />
-              </div>
+          <div className="col-md-6">
+            <label className="form-label">Email</label>
+            <input
+              type="email"
+              className="form-control"
+              name="Email"
+              value={input.Email}
+              onChange={inputHandler}
+            />
+          </div>
 
-              <div className="col-md-6">
-                <label className="form-label">Phone</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="Phone"
-                  value={input.Phone}
-                  onChange={inputHandler}
-                />
-              </div>
+          <div className="col-md-6">
+            <label className="form-label">Phone</label>
+            <input
+              type="text"
+              className="form-control"
+              name="Phone"
+              value={input.Phone}
+              onChange={inputHandler}
+            />
+          </div>
 
-              <div className="col-md-6">
-                <label className="form-label">Date of Birth</label>
-                <input
-                  type="date"
-                  className="form-control"
-                  name="DateOfBirth"
-                  value={input.DateOfBirth}
-                  onChange={inputHandler}
-                />
-              </div>
+          <div className="col-md-6">
+            <label className="form-label">Date of Birth</label>
+            <input
+              type="date"
+              className="form-control"
+              name="DateOfBirth"
+              value={input.DateOfBirth}
+              onChange={inputHandler}
+            />
+          </div>
 
-              <div className="col-md-6">
-                <label className="form-label">Gender</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="Gender"
-                  value={input.Gender}
-                  onChange={inputHandler}
-                />
-              </div>
+          <div className="col-md-6">
+            <label className="form-label">Gender</label>
+            <input
+              type="text"
+              className="form-control"
+              name="Gender"
+              value={input.Gender}
+              onChange={inputHandler}
+            />
+          </div>
 
-              <div className="col-md-6">
-                <label className="form-label">Address</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="Address"
-                  value={input.Address}
-                  onChange={inputHandler}
-                />
-              </div>
+          <div className="col-md-6">
+            <label className="form-label">Address</label>
+            <input
+              type="text"
+              className="form-control"
+              name="Address"
+              value={input.Address}
+              onChange={inputHandler}
+            />
+          </div>
 
-              <div className="col-md-6">
-                <label className="form-label">Blood Group</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="BloodGroup"
-                  value={input.BloodGroup}
-                  onChange={inputHandler}
-                />
-              </div>
+          <div className="col-md-6">
+            <label className="form-label">Blood Group</label>
+            <input
+              type="text"
+              className="form-control"
+              name="BloodGroup"
+              value={input.BloodGroup}
+              onChange={inputHandler}
+            />
+          </div>
 
-              <div className="col-md-6">
-                <label className="form-label">Department</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="Department"
-                  value={input.Department}
-                  onChange={inputHandler}
-                />
-              </div>
+          <div className="col-md-6">
+            <label className="form-label">Department</label>
+            <input
+              type="text"
+              className="form-control"
+              name="Department"
+              value={input.Department}
+              onChange={inputHandler}
+            />
+          </div>
 
-              <div className="col-md-6">
-                <label className="form-label">Year of Study</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="YearOfStudy"
-                  value={input.YearOfStudy}
-                  onChange={inputHandler}
-                />
-              </div>
+          <div className="col-md-6">
+            <label className="form-label">Year of Study</label>
+            <input
+              type="text"
+              className="form-control"
+              name="YearOfStudy"
+              value={input.YearOfStudy}
+              onChange={inputHandler}
+            />
+          </div>
 
-              <div className="col-md-6">
-                <label className="form-label">Camp Name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="CampName"
-                  value={input.CampName}
-                  onChange={inputHandler}
-                />
-              </div>
+          <div className="col-md-6">
+            <label className="form-label">Camp Name</label>
+            <input
+              type="text"
+              className="form-control"
+              name="CampName"
+              value={input.CampName}
+              onChange={inputHandler}
+            />
+          </div>
 
-              <div className="col-md-6">
-                <label className="form-label">Hours Completed</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="HoursCompleted"
-                  value={input.HoursCompleted}
-                  onChange={inputHandler}
-                />
-              </div>
+          <div className="col-md-6">
+            <label className="form-label">Hours Completed</label>
+            <input
+              type="text"
+              className="form-control"
+              name="HoursCompleted"
+              value={input.HoursCompleted}
+              onChange={inputHandler}
+            />
+          </div>
 
-              <div className="col-md-6">
-                <label className="form-label">Unit Number</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="UnitNumber"
-                  value={input.UnitNumber}
-                  onChange={inputHandler}
-                />
-              </div>
+          <div className="col-md-6">
+            <label className="form-label">Unit Number</label>
+            <input
+              type="text"
+              className="form-control"
+              name="UnitNumber"
+              value={input.UnitNumber}
+              onChange={inputHandler}
+            />
+          </div>
 
-              <div className="col-12 text-center">
-                <button className="btn btn-success px-4" onClick={readValue}>
-                  Submit
-                </button>
-              </div>
-            </div>
+          <div className="col-12 text-center mt-3">
+            <button className="btn btn-primary" onClick={readValue}>
+              Add Volunteer
+            </button>
           </div>
         </div>
       </div>
--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,83 +1,76 @@
-
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import Nav from './Nav'
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import NavBar from "./NavBar";
 
 const ViewAll = () => {
+  const [data, changeData] = useState([]);
 
-    const [data, changeData] = useState([])
+  const fetchData = () => {
+    axios
+      .post("http://localhost:3000/view-data")
+      .then((response) => {
+        changeData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    const fetchData = () => {
-        axios.post("http://localhost:3000/view-data")
-            .then((response) => {
-                changeData(response.data)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-    }
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        fetchData()
-    }, [])
+  return (
+    <div>
+      <NavBar />
 
-    return (
-        <div>
+      <div className="container mt-4">
+        <h3 className="text-center mb-3">View Volunteers</h3>
 
+        <div className="table-responsive">
+          <table className="table table-bordered">
+            <thead>
+              <tr style={{ backgroundColor: "white" }}>
+                <th>Volunteer ID</th>
+                <th>Full Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>DOB</th>
+                <th>Gender</th>
+                <th>Address</th>
+                <th>Blood Group</th>
+                <th>Department</th>
+                <th>Year</th>
+                <th>Camp Name</th>
+                <th>Hours</th>
+                <th>Unit No.</th>
+              </tr>
+            </thead>
 
-<Nav/>
-            <div className="container mt-4">
-
-                <h2 className="text-center mb-4">
-                    <b>VIEW VOLUNTEERS</b>
-                </h2>
-
-                <table className="table table-bordered table-hover table-striped">
-                    <thead className="table-dark">
-                        <tr>
-                            <th>Volunteer ID</th>
-                            <th>Full Name</th>
-                            <th>Email</th>
-                            <th>Phone</th>
-                            <th>Date of Birth</th>
-                            <th>Gender</th>
-                            <th>Address</th>
-                            <th>Blood Group</th>
-                            <th>Department</th>
-                            <th>Year of Study</th>
-                            <th>Camp Name</th>
-                            <th>Hours Completed</th>
-                            <th>Unit Number</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        {data.map((value, index) => (
-                            <tr key={index}>
-                                <td>{value.VolunteerID}</td>
-                                <td>{value.FullName}</td>
-                                <td>{value.Email}</td>
-                                <td>{value.Phone}</td>
-                                <td>{value.DateOfBirth}</td>
-                                <td>{value.Gender}</td>
-                                <td>{value.Address}</td>
-                                <td>{value.BloodGroup}</td>
-                                <td>{value.Department}</td>
-                                <td>{value.YearOfStudy}</td>
-                                <td>{value.CampName}</td>
-                                <td>{value.HoursCompleted}</td>
-                                <td>{value.UnitNumber}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-
-                </table>
-
-            </div>
-
+            <tbody>
+              {data.map((value) => (
+                <tr key={value._id} style={{ backgroundColor: "white" }}>
+                  <td>{value.VolunteerID}</td>
+                  <td>{value.FullName}</td>
+                  <td>{value.Email}</td>
+                  <td>{value.Phone}</td>
+                  <td>{value.DateOfBirth}</td>
+                  <td>{value.Gender}</td>
+                  <td>{value.Address}</td>
+                  <td>{value.BloodGroup}</td>
+                  <td>{value.Department}</td>
+                  <td>{value.YearOfStudy}</td>
+                  <td>{value.CampName}</td>
+                  <td>{value.HoursCompleted}</td>
+                  <td>{value.UnitNumber}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ViewAll
+export default ViewAll;
